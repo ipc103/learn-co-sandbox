4e4dbfe490c6df8e0bdb3769e1270b7e43c630eb
Ian Candy <iancandysemail@gmail.com>
2019-10-21 19:54:47 +0000

    Automatically backed up by Learn

---
+++ app.rb
@@ -0,0 +1,12 @@
+names = ["Katherine", "Dorothy", "Mary"]
+
+# person = ["Ian", 45, "Pittsburgh, PA", "New York, NY"]
+
+person = { name: "Ian", age: 45, hometown: "Pittsburgh, PA", current_location: "New York, NY"}
+
+# PERSON
+# Index       |    Value
+#   name           "Ian"
+#   age            45
+#  hometown       "Pittsburgh, PA"
+#current_location "New York, NY"
